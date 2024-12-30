7e0f769d2ec97844fea64e9a20c95dc861fef117
hey-ewan <ewanretorokugbe@gmail.com>
2024-12-30 11:16:30 +0100

    chore: test out transcript verifier

--- execution/src/main.rs
+++ execution/src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+use verifier::{verify_proof, verify_proof_from_json};
+
 fn main() {
-    println!("Hello, world!");
+    // Print the current working directory to see where the program is looking
+    println!("Current directory: {:?}", env::current_dir().unwrap());
+    match verify_proof("fixture/simple_proof.json") {
+        Ok(result) => {
+            println!("-------------------------------------------------------------------");
+            println!(
+                "Successfully verified that the bytes below came from a session with {:?} at {}.",
+                result.server_name, result.time
+            );
+            println!("Note that the bytes which the Prover chose not to disclose are shown as X.");
+            println!();
+            println!("Bytes sent:");
+            println!();
+            println!("{}", result.sent_data);
+            println!();
+            println!("Bytes received:");
+            println!();
+            println!("{}", result.received_data);
+            println!("-------------------------------------------------------------------");
+        }
+        Err(e) => eprintln!("Verification failed: {}", e),
+    }
+    // replace empty string with raw json string
+    match verify_proof_from_json("") {
+        Ok(result) => {
+            println!("-------------------------------------------------------------------");
+            println!(
+                "Successfully verified that the bytes below came from a session with {:?} at {}.",
+                result.server_name, result.time
+            );
+            println!("Note that the bytes which the Prover chose not to disclose are shown as X.");
+            println!();
+            println!("Bytes sent:");
+            println!();
+            println!("{}", result.sent_data);
+            println!();
+            println!("Bytes received:");
+            println!();
+            println!("{}", result.received_data);
+            println!("-------------------------------------------------------------------");
+        }
+        Err(e) => eprintln!("Verification failed: {}", e),
+    }
 }
