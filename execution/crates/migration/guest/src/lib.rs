0a1a6bec2f2ceaad5d76f837249207b250eda39e
hey-ewan <ewanretorokugbe@gmail.com>
2025-01-01 14:59:39 +0100

    feat: transcript verifier zkvm guest code

--- execution/crates/migration/guest/src/lib.rs
+++ execution/crates/migration/guest/src/lib.rs
@@ -2,20 +2,38 @@
 pub mod utils;
 pub use utils::{Input, Output};
 
-use sha3::{Digest, Keccak256};
-use verifier::verify_proof;
+use verifier::verify_proof_from_json;
 
 #[jolt::provable]
 fn migrate(pre_image: Input) -> Output {
-    let mut hash = pre_image.input;
-    for _ in 0..pre_image.num_iters {
-        let mut hasher = Keccak256::new();
-        hasher.update(pre_image.input);
-        let res = &hasher.finalize();
-        hash = Into::<[u8; 32]>::into(*res);
+    fn print_verification_result(result: &verifier::VerificationResult) {
+        println!("-------------------------------------------------------------------");
+        println!(
+            "Successfully verified that the bytes below came from a session with {:?} at {}.",
+            result.server_name, result.time
+        );
+        println!("Note that the bytes which the Prover chose not to disclose are shown as X.");
+        println!();
+        println!("Bytes sent:\n\n{}\n", result.sent_data);
+        println!("Bytes received:\n\n{}", result.received_data);
+        println!("-------------------------------------------------------------------");
     }
 
-    let output = Output { output: hash };
+    let output = match verify_proof_from_json(pre_image.transcript) {
+        Ok(result) => {
+            print_verification_result(&result);
+            Output {
+                server_name: result.server_name,
+                time: result.time,
+                sent_data: result.sent_data,
+                received_data: result.recieved_data,
+            }
+        }
+        Err(e) => {
+            eprintln!("Verification failed: {}", e);
+            Output::default()
+        }
+    };
 
     output
 }
