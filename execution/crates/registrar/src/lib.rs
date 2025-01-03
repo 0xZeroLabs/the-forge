bbaa20913af62a9afa54a3288b6f97555534c567
hey-ewan <ewanretorokugbe@gmail.com>
2025-01-03 01:29:27 +0100

    chore: added params and return to registrar func

--- execution/crates/registrar/src/lib.rs
+++ execution/crates/registrar/src/lib.rs
@@ -1,5 +1,6 @@
-use alloy::{primitives::address, providers::ProviderBuilder, sol};
+use alloy::{primitives::Address, providers::ProviderBuilder, rpc::types::TransactionReceipt, sol};
 use eyre::Result;
+use IPARegistrar::IPMetadata;
 
 sol!(
     #[allow(missing_docs)]
@@ -8,13 +9,33 @@ sol!(
     "fixture/abi/IPARegistrar.json"
 );
 
-pub async fn register_ip() -> eyre::Result<()> {
+pub async fn register_ip(
+    address: Address,
+    name: String,
+    ip_metatdata_uri: String,
+    ip_metadata: String,
+    nft_metadata_uri: String,
+    nft_metadata: String,
+) -> Result<TransactionReceipt> {
     let rpc_url = "https://rpc.odyssey.storyrpc.io".parse()?;
 
     let provider = ProviderBuilder::new().on_http(rpc_url);
 
-    let contract = IPARegistrar::new(address!("registrar_address"), provider);
+    let contract = IPARegistrar::new(address, provider);
 
-    contract.register().send().await?;
-    Ok(())
+    let imetadata = IPMetadata {
+        name,
+        ipMetadataURI: ip_metatdata_uri,
+        ipMetadata: ip_metadata,
+        nftMetadataURI: nft_metadata_uri,
+        nftMetadata: nft_metadata,
+    };
+
+    let result = contract
+        .register(address, imetadata)
+        .send()
+        .await?
+        .get_receipt()
+        .await?;
+    Ok(result)
 }
