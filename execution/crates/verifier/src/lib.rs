1c86ac6a1f17f8af0aa1be6fe69f5196c3801301
hey-ewan <ewanretorokugbe@gmail.com>
2025-01-11 12:08:37 +0100

    chore: enable serde Serialize for VerificationResult

--- execution/crates/verifier/src/lib.rs
+++ execution/crates/verifier/src/lib.rs
@@ -1,4 +1,5 @@
 use elliptic_curve::pkcs8::DecodePublicKey;
+use serde::Serialize;
 use std::{str, time::Duration};
 use tlsn_core::{
     proof::{SessionProof, TlsProof},
@@ -83,9 +84,10 @@ pub fn verify_proof_from_json(
 }
 
 /// Struct to hold the verification results
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct VerificationResult {
     pub server_name: ServerName,
+    #[serde(serialize_with = "serialize_datetime")]
     pub time: chrono::DateTime<chrono::Utc>,
     pub sent_data: String,
     pub received_data: String,
@@ -95,4 +97,15 @@ pub struct VerificationResult {
 fn notary_pubkey() -> p256::PublicKey {
     let pem_file = str::from_utf8(include_bytes!("../../../fixture/notary/notary.pub")).unwrap();
     p256::PublicKey::from_public_key_pem(pem_file).unwrap()
+}
+
+/// Custom serialization function for DateTime
+fn serialize_datetime<S>(
+    date: &chrono::DateTime<chrono::Utc>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&date.to_rfc3339())
 }
